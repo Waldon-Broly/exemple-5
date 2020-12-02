6450c14cfe041eacad050002ba75d58d14cc205a
Waldon-Broly <wail.riachi@gmail.com>
2020-12-02 14:47:23 +0100

    extest

--- src/main/java/epsi/CalculatorService.java
+++ src/main/java/epsi/CalculatorService.java
@@ -15,6 +15,6 @@ public final class CalculatorService {
      * @return the result of a + b
      */
     public int add(Collection<Integer> numbers) {
-        return numbers.stream().reduce(0, CalculatorAlgo::add);
+        return numbers.stream().reduce(1, CalculatorAlgo::add);
     }
 }
